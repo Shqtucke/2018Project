e2247ab34961edbba69e90f270f23ff1819dd89d
Shaun Tucker <tucke@Shauns-MacBook-Air.local>
2018-01-28 17:36:15 -0500

    ContactVC added

---
+++ PhaseTwo/Controller/ContactVC.swift
@@ -0,0 +1,25 @@
+//
+//  ContactVC.swift
+//  PhaseTwo
+//
+//  Created by Shaun Tucker on 1/28/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ContactVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
